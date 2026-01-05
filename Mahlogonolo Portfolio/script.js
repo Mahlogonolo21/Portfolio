9bcff8e8ef6767d16525324397d686c2fcdc5458
MAHLOGONOLO <177832004+Mahlogonolo21@users.noreply.github.com>
2026-01-05 08:06:39 +0200

    Update script.js

--- Mahlogonolo Portfolio/script.js
+++ Mahlogonolo Portfolio/script.js
@@ -1,7 +1,8 @@
-// Close menu when a navbar link is clicked
+// Close menu when a navbar link is clickedd
 document.querySelectorAll('.navbar a').forEach(link => {
     link.addEventListener('click', () => {
         menuIcon.classList.remove('bx-x');
         navbar.classList.remove('active');
     });
+
 });
