dab1297c7f2e3573aea2b4994643bdd59fcc6a92
MAHLOGONOLO <177832004+Mahlogonolo21@users.noreply.github.com>
2026-04-22 20:35:11 +0200

    Update script.js

--- Mahlogonolo Portfolio/script.js
+++ Mahlogonolo Portfolio/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.navbar a').forEach(link => {
     link.addEventListener('click', () => {
         menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
+        navbar.classList.remove('active'); 
     });
 
 });
