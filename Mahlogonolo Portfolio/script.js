f08cd37798092b8f734e1d74abeb54ca6d5b9c73
MAHLOGONOLO <177832004+Mahlogonolo21@users.noreply.github.com>
2026-04-29 09:31:05 +0200

    Refactor navbar click event listener code

--- Mahlogonolo Portfolio/script.js
+++ Mahlogonolo Portfolio/script.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.navbar a').forEach(link => {
     link.addEventListener('click', () => {
         menuIcon.classList.remove('bx-x'); 
         navbar.classList.remove('active'); 
-    });
+    }); 
 
 });
   
